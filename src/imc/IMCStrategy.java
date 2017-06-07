fc09dd099a1aa46b1867a1977993b188cc4f1fd9
Gabriel Lima <gabrielcastroslima@gmail.com>
2017-06-07 13:03:31 -0300

    Remove pastas desnecessárias

--- src/imc/IMCStrategy.java
+++
@@ -1,7 +0,0 @@
-//Padrão de projeto Strategy
-
-package imc;
-
-public interface IMCStrategy {
-	public String calcularIMC(double imc, String sexo, int idade);
-}
--- src/imc/MensagemFactory.java
+++
@@ -1,7 +0,0 @@
-//Padrão de projeto Factory
-
-package imc;
-
-public interface MensagemFactory {
-	String emitirMensagem();
-}
